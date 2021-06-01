4047b41063fd7b5e42457c3add94c13d8b587c02
Tiago Silva <tiago@tiago.blog.br>
2021-05-31 21:02:12 -0300

    Adição de grupos de usuários.

---
+++ App/Model/UsuarioGrupoModel.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Model;
+
+use App\DAO\UsuarioGrupoDAO;
+use Exception;
+
+class UsuarioGrupoModel extends Model
+{
+    public $id;
+    public $descricao;
+    public $data_cadastro;
+
+    public $rows;
+    public $rows_count;
+
+    public $success_message;
+    public $error_message;
+
+
+    public function __construct()
+    {
+        self::$dao = new UsuarioGrupoDAO();
+    }
+
+    public function getAllRows()
+    {
+        $this->rows = self::$dao->getAllRows();
+        $this->rows_count = count($this->rows);
+    }
+
+
+    public function getById(int $id)
+    {
+        $obj = self::$dao->getById($id);
+
+        $this->id = $obj->id;
+        $this->descricao = $obj->descricao;
+        $this->data_cadastro = $obj->data_cadastro;
+    }
+
+    public function save()
+    {
+        if($this->id == null)
+        {
+            self::$dao->insert($this);
+            $this->success_message = "Grupo de Usuários Inserido com Sucesso!";
+
+        } else {
+            self::$dao->update($this);
+            $this->success_message = "Grupo de Usuários Editado com Sucesso!";
+        }
+    }
+
+    public function delete(int $id)
+    {
+        try
+        {
+            $obj = self::$dao->delete($id);
+        } catch(Exception $e) {
+            $this->error_message = $e->getMessage();
+        }        
+    }
+
+}
